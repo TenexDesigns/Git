4782bec5a97eb2717d8f6c6913bbe1384189b16d
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-19 13:56:14 +0300

    Create 2_What_is_Git.java

---
+++ 1_What_is_Git/2_What_is_Git.java
@@ -0,0 +1,31 @@
+**Git**
+
+Git is an open-source distributed version control system, available for free under the GNU General Public License version 2. It's primarily used by developers to manage their source code, recording changes to files over time and ensuring the integrity of those changes at each stage of processing [Source 7](https://www.techtarget.com/searchitoperations/definition/Git). 
+
+Unlike other version control systems (VCS) that store information as a list of file-based changes, Git thinks about its data more like a series of snapshots of a miniature filesystem. Every time you commit or save the state of your project, Git takes a picture of what all your files look like at that moment and stores a reference to that snapshot [Source 4](https://www.git-scm.com/book/en/v2/Getting-Started-What-is-Git%3F). 
+
+**Version Control**
+
+Version control, also known as versioning or source control, is the practice of managing and tracking changes to source code. It keeps a detailed account of every modification made to the code, ensuring that these changes are both trackable and reversible [Source 17](https://blog.logrocket.com/product-management/version-control-systems-definition-types/). 
+
+A version control system (VCS) gives each contributor a unified and consistent view of a project, surfacing work that's already in progress. Seeing a transparent history of changes, who made them, and how they contribute to the development of a project helps team members stay aligned while working independently [Source 11](https://docs.github.com/en/get-started/using-git/about-git).
+
+**Repository**
+
+A repository, or Git project, encompasses the entire collection of files and folders associated with a project, along with each file's revision history. The file history appears as snapshots in time called commits, and the commits can be organized into multiple lines of development called branches. Because Git is a distributed version control system (DVCS), repositories are self-contained units and anyone who has a copy of the repository can access the entire codebase and its history [Source 11](https://docs.github.com/en/get-started/using-git/about-git).
+
+**Centralized and Distributed Version Control Systems**
+
+Version control systems come in two main forms, each with its own set of advantages and drawbacks:
+
+1. Centralized Version Control Systems (CVCS): In a CVCS, there's a single, centralized repository, and users get their own working copy of the files without the full version history. Examples include Subversion (SVN) and Perforce.
+
+2. Distributed Version Control Systems (DVCS): In a DVCS, every user has a complete copy of the repository, including its full history. This means that even if the central repository becomes corrupted, it’s not a problem because any local servers can send a copy of the history and repository to other local servers or back to the central server. Examples include Git and Mercurial [Source 17](https://blog.logrocket.com/product-management/version-control-systems-definition-types/).
+
+**Tracking History and Collaborative Work**
+
+Git, version control, and repositories work together to help track history and facilitate collaborative work. Git records changes to files over time, allowing users to revert to earlier versions and compare different versions at the file level. They can also branch and merge files to support independent development efforts and nonlinear workflows. 
+
+Version control systems keep a detailed account of every modification made to the code, ensuring that these changes are both trackable and reversible. They give each contributor a unified and consistent view of a project, helping team members stay aligned while working independently. 
+
+Repositories encompass the entire collection of files and folders associated with a project, along with each file's revision history. This makes it possible for anyone who has a copy of the repository to access the entire codebase and its history, facilitating collaboration [Sources 4, 7, 11](https://www.git-scm.com/book/en/v2/Getting-Started-What-is-Git%3F, https://www.techtarget.com/searchitoperations/definition/Git, https://docs.github.com/en/get-started/using-git/about-git).
