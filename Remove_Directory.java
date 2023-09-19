7a8d13539a6c69e9864771a6bca4b7209009421f
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-19 16:53:58 +0300

    Create Remove_Directory.java

---
+++ Remove_Directory.java
@@ -0,0 +1,13 @@
+Yes, you can use the `rm` command to remove directories in a Unix-like or Linux operating system. To remove a directory, including all its contents (subdirectories and files), you can use the `rm` command with the `-r` (or `-rf`) option. Here's the syntax:
+
+```bash
+rm -r directory_name
+```
+
+Replace `directory_name` with the name of the directory you want to remove. Here's an example:
+
+```bash
+rm -r my_directory
+```
+
+Please exercise caution when using the `rm -r` command because it will permanently delete the specified directory and its contents, and there is typically no way to recover them. Make sure you have a backup or are confident that you want to delete the directory before running this command.
