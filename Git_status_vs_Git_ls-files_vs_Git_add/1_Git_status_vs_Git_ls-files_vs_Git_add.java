1779f411d088d772baae65827f90931724210f98
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-19 21:26:59 +0300

    Update 1_Git_status_vs_Git_ls-files_vs_Git_add.java

--- Git_status_vs_Git_ls-files_vs_Git_add/1_Git_status_vs_Git_ls-files_vs_Git_add.java
+++ Git_status_vs_Git_ls-files_vs_Git_add/1_Git_status_vs_Git_ls-files_vs_Git_add.java
@@ -37,6 +37,55 @@ To summarize, `git status` and `git ls-files` help you inspect the status of you
 
 
 
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+   **git status**
+
+The `git status` command displays the state of the working directory and the staging area. It shows which changes have been staged, which haven’t, and which files aren’t being tracked by Git. It does not show any information regarding the committed project history. The `git status` command is typically used to check which files have changes that are ready to be committed [Source 1](https://www.atlassian.com/git/tutorials/inspecting-a-repository), [Source 10](https://www.w3docs.com/learn-git/git-status.html).
+
+**Usage**
+
+```shell
+git status
+```
+
+**git ls-files**
+
+The `git ls-files` command shows information about files in the index and the working tree. It merges the file listing in the index with the actual working directory list, and shows different combinations of the two. This command can be useful to see which files are currently being tracked by Git [Source 4](https://git-scm.com/docs/git-ls-files).
+
+**Usage**
+
+```shell
+git ls-files
+```
+
+**git add**
+
+The `git add` command adds a change in the working directory to the staging area. It tells Git that you want to include updates to a particular file in the next commit. However, `git add` doesn't really affect the repository in any significant way—changes are not actually recorded until you run `git commit`. The `git add` command is used to stage changes for the next commit [Source 12](https://www.atlassian.com/git/tutorials/saving-changes).
+
+**Usage**
+
+```shell
+git add <file or directory>
+```
+
+**git commit**
+
+The `git commit` command captures a snapshot of the project's currently staged changes. Committed snapshots can be thought of as “safe” versions of a project—Git will never change them unless you explicitly ask it to. The `git commit` command is used to save the staged changes to the repository [Source 9](https://www.atlassian.com/git/tutorials/saving-changes/git-commit).
+
+**Usage**
+
+```shell
+git commit -m "Your commit message"
+```
+
+**How Git Handles Changes**
+
+When you edit, delete, or add files in your working directory, these changes are not immediately included in your repository. You need to tell Git which changes you want to include in the next commit. You do this by staging the changes with the `git add` command. Staged changes are changes that are ready to be committed to the repository. When you run `git commit`, Git takes all the changes that have been staged, creates a new commit object, and updates the repository to include the new commit [Source 9](https://www.atlassian.com/git/tutorials/saving-changes/git-commit) [Source 12](https://www.atlassian.com/git/tutorials/saving-changes).
 
 
 
@@ -45,6 +94,52 @@ To summarize, `git status` and `git ls-files` help you inspect the status of you
 
 
 
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+   **git status** reveals the state of your Git repository. It tells you which files are in the working directory, which files are in the staging area, and which files are uncommitted.
+
+**git ls-files** reveals the files in the staging area. It is a list of all of the files that will be included in the next commit.
+
+**git add** adds files from the working directory to the staging area. This does not remove the files from the working directory. Instead, it simply marks them for inclusion in the next commit.
+
+**git commit** creates a snapshot of the files in the staging area and stores it in the Git repository. This removes the files from the staging area but does not remove them from the working directory.
+
+**When you edit, delete, or remove files, you do it to the working directory.** The staging area and the snapshot version of the project are not affected until you run the `git add` command.
+
+Here is an example:
+
+```
+# Initialize a new Git repository
+git init
+
+# Create a new file called README.md
+echo "This is the README.md file." > README.md
+
+# Add the README.md file to the staging area
+git add README.md
+
+# Commit the changes
+git commit -m "Add README.md file"
+
+# Make some changes to the README.md file
+echo "This is an updated version of the README.md file." > README.md
+
+# The README.md file is now in the working directory but not in the staging area.
+
+# To add the modified README.md file to the staging area, you would run the following command:
+git add README.md
+
+# To commit the changes, you would run the following command:
+git commit -m "Update README.md file"
+```
+
+Now you have two commits in your Git repository. The first commit is a snapshot of your project when you first created it. The second commit is a snapshot of your project after you made changes to the README.md file.
+
+Git is a powerful tool that can help you to track the changes to your project and manage your project history. I hope this helps!
+
+   
 
 
   
