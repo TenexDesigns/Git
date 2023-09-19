67acb491ee3c803b929c9f3958fe8a211ac5650b
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-19 13:57:21 +0300

    Create 3_What_is_Git.java

---
+++ 1_What_is_Git/3_What_is_Git.java
@@ -0,0 +1,44 @@
+**Git** is a free and open-source distributed version control system (DVCS) designed to handle everything from small to very large projects with speed and efficiency. It is easy to learn and has a tiny footprint with lightning fast performance. It outclasses SCM tools like Subversion, CVS, Perforce, and ClearCase with features like cheap local branching, convenient staging areas, and multiple workflows.
+
+**Version control** is a system that records changes to a file or set of files over time so that you can recall specific versions later. This is useful for many reasons, such as:
+
+* Tracking the history of a project and seeing how it has changed over time
+* Collaborating with other people on the same project
+* Recovering from mistakes or lost work
+
+A **repository** is a central place where all of the files in a project are stored and version controlled. It can be hosted on a remote server or on your local machine.
+
+Git, version control, and repositories work together to help you track the history of your project and collaborate with others. Git does this by storing a complete history of all changes to your files in the repository. This allows you to see exactly what changes were made, when they were made, and by whom. It also allows you to easily revert to previous versions of your files if necessary.
+
+**Centralized version control systems** (CVCS) store the repository on a central server. All users must check out the latest version of the files from the server before they can make changes. This can make it difficult to collaborate with others, especially if you are working on different versions of the same file at the same time.
+
+**Distributed version control systems** (DVCS) store a complete copy of the repository on each user's computer. This allows users to make changes to their files without having to check them out from a central server. This makes it much easier to collaborate with others, and it also allows users to work on their projects even if they do not have an internet connection.
+
+**Examples of centralized version control systems:**
+
+* Subversion (SVN)
+* CVS
+* Perforce
+* ClearCase
+
+**Examples of distributed version control systems:**
+
+* Git
+* Mercurial
+* Darcs
+
+Git is the most popular distributed version control system in use today. It is used by millions of developers all over the world to manage their projects.
+
+Here is an example of how Git, version control, and repositories can be used to track the history of a project and collaborate with others:
+
+1. A developer creates a new Git repository and clones it to their local machine.
+2. The developer makes some changes to the files in the repository and commits those changes.
+3. The developer pushes the changes to the remote repository.
+4. Another developer clones the repository to their local machine.
+5. The second developer pulls the latest changes from the remote repository.
+6. The second developer makes some changes to the files in the repository and commits those changes.
+7. The second developer pushes the changes to the remote repository.
+
+Both developers can now see the latest changes to the project and they can easily merge their changes together if necessary.
+
+Git, version control, and repositories are essential tools for any developer. They help you to track the history of your project, collaborate with others, and recover from mistakes.
