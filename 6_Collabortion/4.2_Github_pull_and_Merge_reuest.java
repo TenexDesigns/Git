b718b51b3779b2239edb017193f72185686a5c4c
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-20 12:10:05 +0300

    Create 4.2_Github_pull_and_Merge_reuest.java

---
+++ 6_Collabortion/4.2_Github_pull_and_Merge_reuest.java
@@ -0,0 +1,44 @@
+A GitHub pull request (PR) is a way to propose changes to a codebase. It is a way to get feedback from other developers and to collaborate on changes before they are merged into the main branch of the repository.
+
+To create a PR, you first need to create a new branch in your repository. This branch will contain your proposed changes. Once you have made your changes, you can push the branch to the remote repository. Then, you can create a PR to merge your branch into the main branch.
+
+The PR will show a list of the changes that you have made. Other developers can then review your changes and leave comments. You can also respond to their comments and make further changes to your code.
+
+Once you are happy with your changes, you can ask for a review from another developer. Once the reviewer has approved your changes, you can merge the PR into the main branch.
+
+**Here are the steps to create a pull request:**
+
+1. Create a new branch in your repository.
+2. Make your changes to the branch.
+3. Push the branch to the remote repository.
+4. Create a pull request to merge your branch into the main branch.
+5. Get feedback from other developers and make any necessary changes.
+6. Merge the pull request into the main branch.
+
+**Here are the benefits of using pull requests:**
+
+* They allow you to get feedback from other developers before you merge your changes into the main branch.
+* They allow you to collaborate on changes with other developers.
+* They provide a record of all of the changes that have been made to the codebase.
+
+**Here are the steps to merge a pull request:**
+
+1. Go to the pull request page.
+2. Click the **Merge** button.
+3. Select the merge method.
+4. Click the **Merge pull request** button.
+
+**Conclusion**
+
+GitHub pull requests are a powerful tool for collaborating on code. They allow you to get feedback from other developers and to merge your changes into the main branch in a controlled way.
+
+
+
+
+
+
+
+
+
+
+  
