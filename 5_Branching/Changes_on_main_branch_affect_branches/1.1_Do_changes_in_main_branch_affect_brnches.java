bbcfb00cebf8841ee54c1b2a1e4db85f70406fc1
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-20 12:51:23 +0300

    Create 1.1_Do_changes_in_main_branch_affect_brnches.java

---
+++ 5_Branching/Changes_on_main_branch_affect_branches/1.1_Do_changes_in_main_branch_affect_brnches.java
@@ -0,0 +1,29 @@
+Changes in the main branch do not affect the branches directly. Branches are snapshots of the main branch at a specific point in time. This means that changes made to the main branch will not be reflected in the branches until you merge them.
+
+The recommended way to make changes to the main branch is through branching and merging. This allows you to experiment with new features or fix bugs without affecting the main branch. Once you are happy with your changes, you can merge them into the main branch.
+
+Making changes directly to the main branch should be avoided, especially if you are collaborating with other developers. This is because it can make it difficult to track changes and resolve conflicts.
+
+Here is a workflow that you can follow to make changes to the main branch:
+
+1. Create a new branch for your changes.
+2. Make your changes to the branch.
+3. Commit your changes to the branch.
+4. Push the branch to the remote repository.
+5. Create a pull request to merge your branch into the main branch.
+6. Get feedback from other developers and make any necessary changes.
+7. Merge the pull request into the main branch.
+
+This workflow is recommended for both collaborators and maintainers of the project. It allows everyone to stay on the same page and to ensure that changes are made in a controlled way.
+
+Here are some of the benefits of using branching and merging:
+
+* It allows you to experiment with new features or fix bugs without affecting the main branch.
+* It allows you to collaborate with other developers on different parts of the project at the same time.
+* It makes it easier to track changes and resolve conflicts.
+* It provides a record of all of the changes that have been made to the codebase.
+
+I hope this explanation is helpful. Please let me know if you have any other questions.
+
+
+
