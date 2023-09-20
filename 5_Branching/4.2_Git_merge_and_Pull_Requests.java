ef3707bcc9dbaab5ddf899735c73e6e6ce87d352
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-20 12:29:38 +0300

    Create 4.2_Git_merge_and_Pull_Requests.java

---
+++ 5_Branching/4.2_Git_merge_and_Pull_Requests.java
@@ -0,0 +1,42 @@
+A GitHub pull request is a feature that allows developers to propose changes to a project. It is a mechanism for a developer to notify team members that they have completed a feature. Once their feature branch is ready, the developer files a pull request via their GitHub account. This lets everybody involved know that they need to review the code and merge it into the main branch [Source 2](https://www.howtogeek.com/devops/what-are-git-pull-requests-and-how-do-you-use-them/).
+
+Pull requests are more than just a notification—they’re a dedicated forum for discussing the proposed feature. If there are any problems with the changes, teammates can post feedback in the pull request and even tweak the feature by pushing follow-up commits. All of this activity is tracked directly inside of the pull request [Source 8](https://www.atlassian.com/git/tutorials/making-a-pull-request).
+
+To merge a pull request, you can follow these steps:
+
+1. Navigate to the original repository where the pull request has been sent.
+2. Under the repository name, click on "Pull requests".
+3. In the "Pull Requests" list, click on the pull request you would like to merge.
+4. Review the changes ensuring they are correct and won't cause any issues.
+5. Click on "Merge pull request" to merge the changes into the base branch.
+6. Click "Confirm merge".
+
+Remember, only users with write access to the repository can merge pull requests. If you don't have write access, you can request a project maintainer to merge the pull request for you [Source 1](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/incorporating-changes-from-a-pull-request/about-pull-request-merges). 
+
+Lastly, it's important to note that pull requests are a critical part of open-source collaboration. They allow anyone to suggest changes, even if they don't have access to the repository. When a pull request is opened, you can discuss and review the potential changes with collaborators and add follow-up commits before the changes are merged into the base branch [Source 5](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/about-pull-requests).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
