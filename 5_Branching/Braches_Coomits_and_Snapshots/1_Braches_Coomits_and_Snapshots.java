6525a24786776ece6498c686f081f4ae2c3672c7
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-20 13:08:50 +0300

    Create 1_Braches_Coomits_and_Snapshots.java

---
+++ 5_Branching/Braches_Coomits_and_Snapshots/1_Braches_Coomits_and_Snapshots.java
@@ -0,0 +1,36 @@
+
+
+Quiz  -- also, there are branches  and commits and snapshots , we make commits or incrememtal changes to branches, then when we finalize with a branche, we pull and merge it to the main branch,
+  am I correct in my analisys 
+
+
+  ---------------------- ---------------------- ---------------------- ---------------------- ---------------------- ---------------------- ---------------------- ----------------------
+
+Your analysis is mostly correct, but let's clarify a few points:
+
+1. **Branches:**
+   - Branches are separate lines of development within a Git repository. They allow you to work on different features, bug fixes, or experiments independently. Each branch can contain a series of commits representing incremental changes to the code.
+
+2. **Commits:**
+   - Commits are snapshots of your project's files at a specific point in time. Commits capture changes made to the codebase, and each commit has a unique identifier. Commits are the building blocks of your Git history.
+
+3. **Snapshots:**
+   - In Git, when you make a commit, you're essentially creating a snapshot of the project's files at that moment. Commits are not just incremental changes but complete snapshots of the entire project directory at the time of the commit.
+
+Here's how it all ties together:
+
+- When you work in a branch, you make changes to the codebase incrementally by creating commits. Each commit represents a snapshot of the code at a particular state.
+
+- As you work in the branch, you can make many commits, each building on the previous one.
+
+- When you're done with the changes in a branch (e.g., you've completed a feature or fixed a bug), you can create a Pull Request (PR) to merge the branch into the main branch (or another target branch).
+
+- Merging a branch into the main branch combines all the changes made in the branch into the main branch. It doesn't just pull the final result; it brings in all the commits and their snapshots.
+
+So, in summary, branches are used to work on different features or tasks, and commits capture incremental changes within those branches. When a branch is ready, it can be merged into the main branch to incorporate all the changes and their associated snapshots into the main codebase. This allows you to maintain a clean and organized history of your project.
+
+
+
+
+
+  
