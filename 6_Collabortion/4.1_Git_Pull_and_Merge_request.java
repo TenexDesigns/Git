b9330c0947ce7d6e3d379aed30ce6c276fd3d87f
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-20 12:09:00 +0300

    Create 4.1_Git_Pull_and_Merge_request.java

---
+++ 6_Collabortion/4.1_Git_Pull_and_Merge_request.java
@@ -0,0 +1,43 @@
+In GitHub collaboration, a **GitHub Pull Request** (often abbreviated as PR) is a feature that allows contributors to propose changes to a repository hosted on GitHub. Pull Requests are a fundamental part of the open-source and collaborative development workflow on GitHub. Here's how they work:
+
+1. **Creating a Pull Request:**
+   - A contributor forks the repository they want to contribute to. This creates a copy of the repository under their own GitHub account.
+   - The contributor clones their forked repository to their local machine and creates a new branch for their changes.
+   - They make the necessary changes, commit them to the branch, and push the branch to their GitHub repository.
+   - From their GitHub repository, they create a Pull Request to the original repository they want to contribute to.
+
+2. **Review and Discussion:**
+   - Other collaborators and maintainers of the original repository can review the changes proposed in the Pull Request.
+   - They can provide comments, feedback, and ask questions about the changes.
+   - Discussions and code review can take place directly in the Pull Request's comment section.
+
+3. **Continuous Integration and Testing:**
+   - Many projects set up automated Continuous Integration (CI) systems that run tests and checks on Pull Requests to ensure that the proposed changes don't introduce errors or break existing functionality.
+
+4. **Merge Pull Request:**
+   - If the reviewers and maintainers are satisfied with the proposed changes, they can merge the Pull Request into the main branch (e.g., `main` or `master`).
+   - Merging incorporates the changes from the contributor's branch into the main branch of the repository.
+   - Once merged, the changes become part of the project's history.
+
+**GitHub Merge Pull Request** is the action taken by the repository maintainers to integrate the proposed changes from the Pull Request into the main branch. This is typically done after the changes have been reviewed, tested, and approved.
+
+Here are the common steps to merge a Pull Request on GitHub:
+
+1. Review: Collaborators and maintainers review the changes in the Pull Request, provide feedback, and discuss any necessary adjustments.
+
+2. Approve: Once the changes are deemed satisfactory, one or more collaborators or maintainers approve the Pull Request.
+
+3. Merge: A maintainer or collaborator with merge permissions can click the "Merge Pull Request" button on the GitHub interface. They may choose to squash and merge (combining all commits into one), rebase and merge (replaying the branch's commits on top of the target branch), or perform a regular merge, depending on the project's preferences.
+
+4. Confirmation: After merging, GitHub provides a confirmation that the Pull Request has been successfully merged.
+
+5. Cleanup: In some cases, the branch associated with the Pull Request may be deleted to keep the repository tidy.
+
+GitHub Pull Requests provide a structured and collaborative way for contributors to submit changes to a project, making it easier for maintainers and teams to manage code contributions and maintain a clean and organized codebase.
+
+
+
+
+
+
+  
